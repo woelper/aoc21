5e13a3dbad0481b4279e80fa08e921d4e242dd3d
Johann Woelper <woelper@gmail.com>
2021-12-06 10:08:05 +0100

    day6

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,5 +1,5 @@
 use std::{fmt::Debug, str::FromStr};
-
+use rayon::prelude::*;
 #[derive(Debug, Clone)]
 struct LanternFish {
     timer: i32,
@@ -23,6 +23,27 @@ impl LanternFish {
     }
 }
 
+fn sim(fish: &Vec<LanternFish>) -> Vec<LanternFish> {
+    fish
+    .par_iter()
+    .map(|f| f.process())
+    .flat_map(|f| f)
+    .collect()
+}
+
+fn process_days(fish: &Vec<usize>, num_days: usize) -> usize {
+    let mut fish = fish.clone();
+    for _d in 0..num_days {
+        let zeroes = fish[0];
+        fish[0] = 0;
+        fish.rotate_left(1);
+        fish[6] += zeroes;
+        fish[8] += zeroes;
+    }
+
+    fish.iter().sum()
+}
+
 fn main() {
     let mut input: Vec<LanternFish> = include_str!("test_input")
         .split(",")
@@ -31,13 +52,18 @@ fn main() {
         })
         .collect();
 
-    dbg!("second part", input);
+        
+        for _i in 0..80 {
+            input = sim(&input);
+        }
+        dbg!("step", &input.len());
 
-    for _i in 0..10 {
-        let x  = input.clone()
-            .iter()
-            .map(|f| f.process())
-            .flat_map(|f| f)
-            .collect::<Vec<LanternFish>>();
-    }
+        // part 2 - just simulating it takes too much time
+        let input: Vec<usize> = include_str!("input").split(",").map(|c| c.parse().unwrap()).fold(vec![0; 9], |mut acc, v: usize| {
+            acc[v] += 1;
+            acc
+        });
+
+        let x = process_days(&input, 256);
+        dbg!(x);
 }
